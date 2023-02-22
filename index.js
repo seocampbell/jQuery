f5b812195d61cf38f3db5a33710c743c19e44083
stephen.campbell <se.campbell@hotmail.co.uk>
2023-02-22 19:02:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//$(document).ready(function () {
+//    $("h1").css("color", "white");
+//});
+
+//$("h1").css("color", "green");
+
+//$("button").css("color", "blue");
+$("h1").addClass("big-title margin-50");
+
+$("h1").text("bye");
+$("button").html("<em>Hey</em>");
